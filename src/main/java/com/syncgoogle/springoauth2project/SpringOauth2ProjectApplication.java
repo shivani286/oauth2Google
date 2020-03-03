0d10c21783cb390343a5a4bd5febf52eacba81fe
shivani-vnode <shivani.thakur@vdotnode.com>
2020-03-03 08:50:31 +0530

    Spring Config

--- src/main/java/com/syncgoogle/springoauth2project/SpringOauth2ProjectApplication.java
+++ src/main/java/com/syncgoogle/springoauth2project/SpringOauth2ProjectApplication.java
@@ -2,12 +2,19 @@ package com.syncgoogle.springoauth2project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringOauth2ProjectApplication {
+public class SpringOauth2ProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringOauth2ProjectApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringOauth2ProjectApplication.class);
 	}
 
 }
